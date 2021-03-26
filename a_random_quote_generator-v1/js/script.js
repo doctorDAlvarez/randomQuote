34e862f4f1a7b72dfa911a40812d5ef90eae0e05
Diego Alvarez <dendrit83@protonmail.ch>
2021-03-25 23:26:09 -0300

    finally changes, random background color

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -5,27 +5,29 @@
 [*] by Diego Alvarez. @doc on slack.
 [*] March 2021.
 ******************************************/
-/*
-This random quote generator app will display random quotes
-previosuly fetch in the "quotes" array, by using two functions:
-- getRandomQuote() = generate the random quote index for the array.
-- printQuote() = get the data from the array objects and
-insert it to the HTML.
+/**
+* This random quote generator app will display random quotes
+* previosuly fetch from an array of objects, by using two functions:
+* - getRandomQuote() = generate and return the random quote object.
+* - printQuote() = get the data from the object returned and
+* insert it to the HTML.
+*
+* Built-ins used: Math.floor, Math.random.
+**/
 
-Built-ins used: Math.floor, Math.random.
-*/
+// ----------------------------------------------------------------------------
 
-/*
-* quotes array, with an object for each quote.
-* objects properties : quote, source, citation, year, tags.
-* Not every object has all the properties.!
-*/
+/**
+* quotes [array], with an object for each quote.
+* Object properties : quote, source, citation, year, tags.
+* Not every object has all properties.!
+**/
 
 const quotes = [
   {
-    quote: "Never go to a doctor whose office plants have died.",
+    quote: `Never go to a doctor whose office plants have died.`,
     source: "humorist, Erma Bombeck",
-    citation: "in the The Democrat and Chornicle´s Newspaper column",
+    citation: "The Democrat and Chornicle´s Newspaper column",
     year: "1975"
   },
   {
@@ -48,25 +50,25 @@ const quotes = [
     tags: "Medical quotes"
   },
   {
-    quote: "I'm going to make him an offer he can't refuse.",
+    quote: `I'm going to make him an offer he can't refuse.`,
     source: "Marlon Brando",
     citation: "The Godfather Movie",
     year: ""
   },
   {
-    quote: "There's no place like home.",
+    quote: `There's no place like home.`,
     source: "Harold Arlen",
     citation: "",
     year: "1939"
   },
   {
-    quote: "Carpe diem. Seize the day, boys. Make your lives extraordinary.",
+    quote: `Carpe diem. Seize the day, boys. Make your lives extraordinary.`,
     source: "John Keating (Robin Williams)",
     citation: "in the movie, Dead Poets Society",
     year: ""
   },
   {
-    quote: "Life is pain and the enjoyment of love is an anesthetic.",
+    quote: `Life is pain and the enjoyment of love is an anesthetic.`,
     source: "Cesare Paverse",
     citation: "in his book ´This Business of living´, p74",
     year: "1961",
@@ -82,30 +84,33 @@ const quotes = [
   },
 ];
 
+// ----------------------------------------------------------------------------
+
 /**
-* getRandomQuote():
-* Arrow function with no parameters, that generates a random number
+* Function: getRandomQuote()
+* Arrow function with no parameters, generates a random number
 * between 0 and the length of the array, so if we want later to add
-* more quotes the app will still randomize all indexes.
+* more quotes, the app will still randomize all indexes.
 *
 * @return {object} - the random object from the array.
-*/
+**/
 
 const getRandomQuote = () => {
-  let random_index = Math.floor(Math.random() * (quotes.length))
+  let random_index = Math.floor( Math.random() * (quotes.length) );
   return quotes[random_index];
 }
 
+// ----------------------------------------------------------------------------
+
 /**
-* printQuote():
+* Function: printQuote()
 * Arrow function with no parameters, that calls the getRandomQuote()
 * function, then generates and return the HTML template string with the data
-* fetch from the object, and inserted in the
-* "quote-box" element.
+* fetch from the object, and inserted in the "quote-box" element.
 * HTML tags used: <p> and <span>.
 *
 * We checked if the quote object has certain properties before using them.
-*/
+**/
 
 const printQuote = () => {
   const random_quote = getRandomQuote();
@@ -125,13 +130,30 @@ const printQuote = () => {
 
   html += `</p>`;
 
+/**
+* Function: rand_rgb()
+* Takes no parameters, and
+* @return {array} - Array of 3 RGB random colors.
+* This colors are passed to the background style of body tag, so
+* we get a random background with each quote.
+**/
+
+  const rand_rgb = () => {
+    let rgb_arr = [];
+    for (let i = 0; i < 3; i++) {
+      rgb_arr.push(Math.floor(Math.random() * 256));
+    }
+    return rgb_arr;
+  }
+
+  document.querySelector("body").style.backgroundColor = `rgb(${rand_rgb()})`;
   document.getElementById('quote-box').innerHTML = html;
 }
 
 /**
-* setting the setInterval() global method to dislplay a random quote, Every
-* 5 seconds.
-*/
+* setting the setInterval() global method to dislplay a random quote,
+* every 10 seconds.
+**/
 
 setInterval(printQuote, 10000);
 
